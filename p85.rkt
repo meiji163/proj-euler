60efd79f67af3fc053fdef3278f21237f6372be5
meiji163 <mysatellite99@gmail.com>
2022-06-05 18:20:16 -0700

    rackety racketer

---
+++ p85.rkt
@@ -0,0 +1,17 @@
+#lang racket
+
+(define (rect-count n m)
+  (/ (* m (add1 m) n (add1 n)) 4))
+
+(define (solve)
+  (define best 10000)
+  (define ans 0)
+  (for ([n (in-range 10 80)])
+    (for ([m (in-range n 80)])
+      (let ([d (abs (- 2000000 (rect-count n m)))])
+      (if (< d best)
+          (set!-values (best ans) (values d (* m n)))
+          '()))))
+  ans)
+
+(display (solve))y
